36167dd5b66b175577db61a0f84e545e1bab689d
Alex Medearis <alexmedearis@gmail.com>
2014-03-12 16:23:22 -0700

    a

--- scripts/prepare_command.rb
+++ scripts/prepare_command.rb
@@ -1,2 +1,15 @@
 puts Dir.pwd 
-puts "HELLOOOOO WORLD"
+require 'xcodeproj'
+require 'find'
+
+project_path = []
+Find.find('../../') do |path|
+  project_path << path if path =~ /.*\.xcodeproj$/
+end
+puts project_path
+
+project = Xcodeproj::Project.open(path_to_project)
+main_target = project.targets.first
+phase = main_target.new_shell_script_build_phase("Name of your Phase")
+phase.shell_script = "do sth with ${CONFIGURATION_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}/your.file"
+project.save()
