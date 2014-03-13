3bc3655d557727008cc53405e02765d250a3f891
Alex Medearis <alexmedearis@gmail.com>
2014-03-13 10:50:36 -0700

    a

--- scripts/prepare_command.rb
+++ scripts/prepare_command.rb
@@ -1,6 +1,8 @@
 require 'xcodeproj'
 require 'find'
 
+BUILD_PHASE_NAME = "Label Optimizely Views"
+
 # Find main project file
 project_path = []
 Find.find('../../') do |path|
@@ -9,13 +11,23 @@ Find.find('../../') do |path|
   end
   project_path << path if path =~ /.*\.xcodeproj$/
 end
-project_path.delete_if { |x| x.include?("Pods") }
 puts project_path
 
 # Open project
 project = Xcodeproj::Project.open(project_path[0])
+
+# Check if build phase already exists
+phases = main_target.shell_script_build_phases
+phases.each do |phase|
+  if phase.name == BUILD_PHASE_NAME
+    puts "SHORTCUT"
+    return
+  end
+end
+
+# If not, add it
 main_target = project.targets.first
-phase = main_target.new_shell_script_build_phase("Label Optimizely Views")
+phase = main_target.new_shell_script_build_phase(BUILD_PHASE_NAME)
 phase.shell_script = "python \"$SRCROOT/Pods/Optimizely-iOS-SDK/scripts/OptimizelyPrepareNibs.py\""
 
 # Move new script phase to the beginning
