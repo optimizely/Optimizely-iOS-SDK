63124953ef649addfea9bd8ac6d31ad90b743288
Alex Medearis <alexmedearis@gmail.com>
2014-03-12 17:32:13 -0700

    a

--- scripts/prepare_command.rb
+++ scripts/prepare_command.rb
@@ -12,7 +12,7 @@ project = Xcodeproj::Project.open(project_path[0])
 main_target = project.targets.first
 phase = main_target.new_shell_script_build_phase("Label Optimizely Views")
 phase.shell_script = "python \"$SRCROOT/Pods/Optimizely-iOS-SDK/OptimizelyPrepareNibs.py\""
-phases = main_target.shell_script_build_phases
+phases = main_target.build_phases
 popped_phase = phases.pop
 phases.unshift popped_phase
 puts phases
