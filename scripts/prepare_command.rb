c10f9865c26acef90e165012573d365e562d5eb5
Mark Larsen <larse503@gmail.com>
2014-05-28 15:21:30 -0700

    Make the prepare command use the Podfile to find the xcodeproj instead of naively looking in the root directory for it

--- scripts/prepare_command.rb
+++ scripts/prepare_command.rb
@@ -1,23 +1,32 @@
 require 'rubygems'
-require 'xcodeproj'
+require 'cocoapods'
 require 'find'
 
 BUILD_PHASE_NAME = "Label Optimizely Views"
 SHELL_SCRIPT = "python \"$SRCROOT/Pods/Optimizely-iOS-SDK/scripts/OptimizelyPrepareNibs.py\""
 
-# Find main project file
-project_path = []
-Dir['../../*'].each do |fname|
-    project_path << fname if fname =~ /.*\.xcodeproj$/
+# Find main project file by looking in the Podfile declaration
+xcodeproj_path = begin
+  Pod::Command::IPC::Podfile::Pod::Podfile.from_file('../../Podfile').to_hash["target_definitions"].first["user_project_path"]
+rescue
+  begin Pod::Command::IPC::Podfile::Pod::Podfile.from_file('../../podfile').to_hash["target_definitions"].first["user_project_path"] rescue nil end
 end
 
-if(project_path.length == 0)
+if xcodeproj_path
+  xcodeproj_path = "../../" + xcodeproj_path
+else
+  xcodeproj_path = Dir['../../*'].detect do |fname|
+      fname =~ /.*\.xcodeproj$/
+  end
+end
+
+unless xcodeproj_path
   print "Could not locate project, please add the OptimizelyPrepareNibs.py build phase manually"
   abort
 end
 
 # Open project
-project = Xcodeproj::Project.open(project_path[0])
+project = Xcodeproj::Project.open(xcodeproj_path)
 main_target = project.targets.first
 
 # What to install
