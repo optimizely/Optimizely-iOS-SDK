535b502762ba9ec84f4c1c2f275c34a8f0ef0354
Tony Zhang <tony@optimizely.com>
2015-06-25 16:23:42 -0700

    1.0.86

--- TutorialApp/TutorialApp/LiveVariablesViewController.m
+++ TutorialApp/TutorialApp/LiveVariablesViewController.m
@@ -32,8 +32,6 @@ OptimizelyVariableKeyForString(liveVariableString, @"Test");
     self.navigationItem.backBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"" style:UIBarButtonItemStylePlain target:nil action:nil];
     self.title = @"Live Variables";
     self.navigationController.title = @"Live Variables";
-    bool test = [Optimizely boolForKey:liveVariableBool];
-    
     
     // Create list of objects
     self.storeItems = [NSMutableDictionary dictionary];
